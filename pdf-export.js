0fbececfb11bd806735d2237ae68ba03a1d7d24d
tss26 <ordini.tessitoresrl@gmail.com>
2025-07-07 13:57:53 +0200

    Add files via upload

--- pdf-export.js
+++ pdf-export.js
@@ -1,95 +1,20 @@
 
 document.getElementById("generaPdf").addEventListener("click", () => {
   const nota = document.getElementById("nota").value || "Preventivo";
-  const uploadBoxes = document.querySelectorAll(".upload-box");
-
-  const container = document.createElement("div");
-  const title = document.createElement("h2");
-  title.innerText = nota;
-  container.appendChild(title);
-
-  const table = document.createElement("table");
-  table.style.width = "100%";
-  table.style.borderCollapse = "collapse";
-  table.style.marginTop = "20px";
-  table.innerHTML = `
-    <thead>
+  const contenutoTest = `
+    <h1>${nota}</h1>
+    <table border="1" style="width:100%; border-collapse:collapse;">
       <tr>
-        <th style='border:1px solid #000;padding:5px;'>Tipo</th>
-        <th style='border:1px solid #000;padding:5px;'>Immagine</th>
-        <th style='border:1px solid #000;padding:5px;'>Descrizione</th>
+        <th>Tipo</th>
+        <th>Immagine</th>
+        <th>Descrizione</th>
       </tr>
-    </thead>
-    <tbody></tbody>
+      <tr>
+        <td>Ricamo lato cuore</td>
+        <td>Nessuna immagine</td>
+        <td>Esempio descrizione</td>
+      </tr>
+    </table>
   `;
-
-  const tbody = table.querySelector("tbody");
-  let immaginiDaCaricare = 0;
-
-  uploadBoxes.forEach(box => {
-    const tipo = box.querySelector("h4").innerText;
-    const imgInput = box.querySelector("input[type='file']");
-    const descInput = box.querySelector("input[type='text']");
-    const row = document.createElement("tr");
-
-    const tdTipo = document.createElement("td");
-    tdTipo.style.border = "1px solid #000";
-    tdTipo.style.padding = "5px";
-    tdTipo.innerText = tipo;
-
-    const tdImg = document.createElement("td");
-    tdImg.style.border = "1px solid #000";
-    tdImg.style.padding = "5px";
-
-    const tdDesc = document.createElement("td");
-    tdDesc.style.border = "1px solid #000";
-    tdDesc.style.padding = "5px";
-    tdDesc.innerText = descInput.value;
-
-    const file = imgInput.files[0];
-    if (file) {
-      immaginiDaCaricare++;
-      const reader = new FileReader();
-      reader.onload = () => {
-        const img = document.createElement("img");
-        img.src = reader.result;
-        img.style.maxWidth = "100px";
-        img.style.maxHeight = "100px";
-        tdImg.appendChild(img);
-        row.appendChild(tdTipo);
-        row.appendChild(tdImg);
-        row.appendChild(tdDesc);
-        tbody.appendChild(row);
-
-        immaginiDaCaricare--;
-        if (immaginiDaCaricare === 0) {
-          container.appendChild(table);
-          generaPdf(container, nota);
-        }
-      };
-      reader.readAsDataURL(file);
-    } else {
-      tdImg.innerText = "Nessuna immagine";
-      row.appendChild(tdTipo);
-      row.appendChild(tdImg);
-      row.appendChild(tdDesc);
-      tbody.appendChild(row);
-    }
-  });
-
-  if (immaginiDaCaricare === 0) {
-    container.appendChild(table);
-    generaPdf(container, nota);
-  }
+  html2pdf().from(contenutoTest).save(`${nota.replace(/\s+/g, "_")}.pdf`);
 });
-
-function generaPdf(contentNode, fileName) {
-  const opt = {
-    margin: 0.5,
-    filename: `${fileName.replace(/\s+/g, "_")}.pdf`,
-    image: { type: "jpeg", quality: 0.98 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: "in", format: "letter", orientation: "portrait" }
-  };
-  html2pdf().from(contentNode).set(opt).save();
-}
