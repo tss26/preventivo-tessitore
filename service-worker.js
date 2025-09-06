2cde3a87242f1bba3b68e0c44215545384c1e89b
tss26 <ordini.tessitoresrl@gmail.com>
2025-09-06 09:54:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,36 @@
+const CACHE_NAME = "preventivo-app-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "pdf-export.js",
+  "autocomplete.js",
+  "icon-192.png",
+  "icon-512.png",
+  "manifest.json"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+
+
+
+/*
 const CACHE_NAME = "preventivo-app-cache-v1";
 const urlsToCache = [
   "index.html",
@@ -24,4 +56,4 @@ self.addEventListener("fetch", (event) => {
       return response || fetch(event.request);
     })
   );
-});
+});*/
