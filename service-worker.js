85d3f429bc741f4422c207a3a0f6e9c040d2eede
tss26 <ordini.tessitoresrl@gmail.com>
2026-02-21 10:15:07 +0100

    Change cache version to v6
    Updated cache version to force cache refresh.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // AGGIORNATO: Cambia la versione per forzare l'aggiornamento della cache
-const CACHE_NAME = "preventivo-app-cache-v5"; 
+const CACHE_NAME = "preventivo-app-cache-v6"; 
 
 const urlsToCache = [
   "index.html",     // serve ancora
